7918a323164cb96bca96ae5c1ddc0c2764c75e00
IvqryDev <noah7365@outlook.com>
2026-06-14 01:51:50 -0500

    Completely reworked potions and alcohol bugfixes.

---
+++ kubejs/startup_scripts/alcohol_rework.js
@@ -0,0 +1,386 @@
+const $MobEffectInstance = Java.loadClass('net.minecraft.world.effect.MobEffectInstance')
+
+ItemEvents.modification(event => {
+    event.modify('brewinandchewin:beer', item => {
+        const foodComponent = item.get('food')
+        const originalNutrition = foodComponent.nutrition()
+        const originalSaturation = foodComponent.saturation()
+        item.setFood({
+            nutrition: originalNutrition,
+            saturation: originalSaturation,
+            canAlwaysEat: true,
+            eatSeconds: 1.6,
+            effects: [
+                {
+                    probability: 1,
+                    effectSupplier: () =>
+                        new $MobEffectInstance(
+                            'minecraft:strength',
+                            6000, /* 5 Minutes */
+                            0, /* Level 1 */
+                            true, /* Ambience */
+                            false /* Particles */
+                        ),
+                },
+            ]
+        })
+    })
+    event.modify('brewinandchewin:saccharine_rum', item => {
+        const foodComponent = item.get('food')
+        const originalNutrition = foodComponent.nutrition()
+        const originalSaturation = foodComponent.saturation()
+        item.setFood({
+            nutrition: originalNutrition,
+            saturation: originalSaturation,
+            canAlwaysEat: true,
+            eatSeconds: 1.6,
+            effects: [
+                {
+                    probability: 1,
+                    effectSupplier: () =>
+                        new $MobEffectInstance(
+                            'minecraft:instant_health',
+                            1, /* Instant */
+                            1, /* Level 2 */
+                            true, /* Ambience */
+                            false /* Particles */
+                        ),
+                },
+            ]
+        })
+    })
+    event.modify('brewinandchewin:salty_folly', item => {
+        const foodComponent = item.get('food')
+        const originalNutrition = foodComponent.nutrition()
+        const originalSaturation = foodComponent.saturation()
+        item.setFood({
+            nutrition: originalNutrition,
+            saturation: originalSaturation,
+            canAlwaysEat: true,
+            eatSeconds: 1.6,
+            effects: [
+                {
+                    probability: 1,
+                    effectSupplier: () =>
+                        new $MobEffectInstance(
+                            'minecraft:water_breathing',
+                            6000, /* 5 Minutes */
+                            0, /* Level 1 */
+                            true, /* Ambience */
+                            false /* Particles */
+                        ),
+                },
+            ]
+        })
+    })
+    event.modify('brewinandchewin:glittering_grenadine', item => {
+        const foodComponent = item.get('food')
+        const originalNutrition = foodComponent.nutrition()
+        const originalSaturation = foodComponent.saturation()
+        item.setFood({
+            nutrition: originalNutrition,
+            saturation: originalSaturation,
+            canAlwaysEat: true,
+            eatSeconds: 1.6,
+            effects: [
+                {
+                    probability: 1,
+                    effectSupplier: () =>
+                        new $MobEffectInstance(
+                            'minecraft:night_vision',
+                            6000, /* 5 Minutes */
+                            0, /* Level 1 */
+                            true, /* Ambience */
+                            false /* Particles */
+                        ),
+                },
+            ]
+        })
+    })
+    event.modify('brewinandchewin:bloody_mary', item => {
+        const foodComponent = item.get('food')
+        const originalNutrition = foodComponent.nutrition()
+        const originalSaturation = foodComponent.saturation()
+        item.setFood({
+            nutrition: originalNutrition,
+            saturation: originalSaturation,
+            canAlwaysEat: true,
+            eatSeconds: 1.6,
+            effects: [
+                {
+                    probability: 1,
+                    effectSupplier: () =>
+                        new $MobEffectInstance(
+                            'minecraft:fire_resistance',
+                            6000, /* 5 Minutes */
+                            0, /* Level 1 */
+                            true, /* Ambience */
+                            false /* Particles */
+                        ),
+                },
+            ]
+        })
+    })
+    event.modify('brewinandchewin:egg_grog', item => {
+        const foodComponent = item.get('food')
+        const originalNutrition = foodComponent.nutrition()
+        const originalSaturation = foodComponent.saturation()
+        item.setFood({
+            nutrition: originalNutrition,
+            saturation: originalSaturation,
+            canAlwaysEat: true,
+            eatSeconds: 1.6,
+            effects: [
+                {
+                    probability: 1,
+                    effectSupplier: () =>
+                        new $MobEffectInstance(
+                            'minecraft:invisibility',
+                            6000, /* 5 Minutes */
+                            0, /* Level 1 */
+                            true, /* Ambience */
+                            false /* Particles */
+                        ),
+                },
+            ]
+        })
+    })
+    event.modify('brewinandchewin:pale_jane', item => {
+        const foodComponent = item.get('food')
+        const originalNutrition = foodComponent.nutrition()
+        const originalSaturation = foodComponent.saturation()
+        item.setFood({
+            nutrition: originalNutrition,
+            saturation: originalSaturation,
+            canAlwaysEat: true,
+            eatSeconds: 1.6,
+            effects: [
+                {
+                    probability: 1,
+                    effectSupplier: () =>
+                        new $MobEffectInstance(
+                            'minecraft:haste',
+                            6000, /* 5 Minutes */
+                            0, /* Level 1 */
+                            true, /* Ambience */
+                            false /* Particles */
+                        ),
+                },
+            ]
+        })
+    })
+    event.modify('brewinandchewin:rice_wine', item => {
+        const foodComponent = item.get('food')
+        const originalNutrition = foodComponent.nutrition()
+        const originalSaturation = foodComponent.saturation()
+        item.setFood({
+            nutrition: originalNutrition,
+            saturation: originalSaturation,
+            canAlwaysEat: true,
+            eatSeconds: 1.6,
+            effects: [
+                {
+                    probability: 1,
+                    effectSupplier: () =>
+                        new $MobEffectInstance(
+                            'minecraft:speed',
+                            6000, /* 5 Minutes */
+                            0, /* Level 1 */
+                            true, /* Ambience */
+                            false /* Particles */
+                        ),
+                },
+            ]
+        })
+    })
+    event.modify('brewinandchewin:mead', item => {
+        const foodComponent = item.get('food')
+        const originalNutrition = foodComponent.nutrition()
+        const originalSaturation = foodComponent.saturation()
+        item.setFood({
+            nutrition: originalNutrition,
+            saturation: originalSaturation,
+            canAlwaysEat: true,
+            eatSeconds: 1.6,
+            effects: [
+                {
+                    probability: 1,
+                    effectSupplier: () =>
+                        new $MobEffectInstance(
+                            'minecraft:regeneration',
+                            6000, /* 5 Minutes */
+                            0, /* Level 1 */
+                            true, /* Ambience */
+                            false /* Particles */
+                        ),
+                },
+            ]
+        })
+    })
+    event.modify('brewinandchewin:vodka', item => {
+        const foodComponent = item.get('food')
+        const originalNutrition = foodComponent.nutrition()
+        const originalSaturation = foodComponent.saturation()
+        item.setFood({
+            nutrition: originalNutrition,
+            saturation: originalSaturation,
+            canAlwaysEat: true,
+            eatSeconds: 1.6,
+            effects: [
+                {
+                    probability: 1,
+                    effectSupplier: () =>
+                        new $MobEffectInstance(
+                            'minecraft:jump_boost',
+                            6000, /* 5 Minutes */
+                            0, /* Level 1 */
+                            true, /* Ambience */
+                            false /* Particles */
+                        ),
+                },
+            ]
+        })
+    })
+    event.modify('brewinandchewin:strongroot_ale', item => {
+        const foodComponent = item.get('food')
+        const originalNutrition = foodComponent.nutrition()
+        const originalSaturation = foodComponent.saturation()
+        item.setFood({
+            nutrition: originalNutrition,
+            saturation: originalSaturation,
+            canAlwaysEat: true,
+            eatSeconds: 1.6,
+            effects: [
+                {
+                    probability: 1,
+                    effectSupplier: () =>
+                        new $MobEffectInstance(
+                            'friendsandfoes:reach',
+                            6000, /* 5 Minutes */
+                            0, /* Level 1 */
+                            true, /* Ambience */
+                            false /* Particles */
+                        ),
+                },
+            ]
+        })
+    })
+    event.modify('brewinandchewin:dread_nog', item => {
+        const foodComponent = item.get('food')
+        const originalNutrition = foodComponent.nutrition()
+        const originalSaturation = foodComponent.saturation()
+        item.setFood({
+            nutrition: originalNutrition,
+            saturation: originalSaturation,
+            canAlwaysEat: true,
+            eatSeconds: 1.6,
+            effects: [
+                {
+                    probability: 1,
+                    effectSupplier: () =>
+                        new $MobEffectInstance(
+                            'minecraft:luck',
+                            6000, /* 5 Minutes */
+                            0, /* Level 1 */
+                            true, /* Ambience */
+                            false /* Particles */
+                        ),
+                },
+            ]
+        })
+    })
+    event.modify('brewinandchewin:steel_toe_stout', item => {
+        const foodComponent = item.get('food')
+        const originalNutrition = foodComponent.nutrition()
+        const originalSaturation = foodComponent.saturation()
+        item.setFood({
+            nutrition: originalNutrition,
+            saturation: originalSaturation,
+            canAlwaysEat: true,
+            eatSeconds: 1.6,
+            effects: [
+                {
+                    probability: 1,
+                    effectSupplier: () =>
+                        new $MobEffectInstance(
+                            'minecraft:resistance',
+                            6000, /* 5 Minutes */
+                            0, /* Level 1 */
+                            true, /* Ambience */
+                            false /* Particles */
+                        ),
+                },
+            ]
+        })
+    })
+    event.modify('brewinandchewin:red_rum', item => {
+        const foodComponent = item.get('food')
+        const originalNutrition = foodComponent.nutrition()
+        const originalSaturation = foodComponent.saturation()
+        item.setFood({
+            nutrition: originalNutrition,
+            saturation: originalSaturation,
+            canAlwaysEat: true,
+            eatSeconds: 1.6,
+            effects: [
+                {
+                    probability: 1,
+                    effectSupplier: () =>
+                        new $MobEffectInstance(
+                            'brewinandchewin:raging',
+                            6000, /* 5 Minutes */
+                            0, /* Level 1 */
+                            true, /* Ambience */
+                            false /* Particles */
+                        ),
+                },
+            ]
+        })
+    })
+    event.modify('brewinandchewin:withering_dross', item => {
+        const foodComponent = item.get('food')
+        const originalNutrition = foodComponent.nutrition()
+        const originalSaturation = foodComponent.saturation()
+        item.setFood({
+            nutrition: originalNutrition,
+            saturation: originalSaturation,
+            canAlwaysEat: true,
+            eatSeconds: 1.6,
+            effects: [
+                {
+                    probability: 1,
+                    effectSupplier: () =>
+                        new $MobEffectInstance(
+                            'minecraft:strength',
+                            3600, /* 3 Minutes */
+                            1, /* Level 2 */
+                            true, /* Ambience */
+                            false /* Particles */
+                        ),
+                },
+                {
+                    probability: 1,
+                    effectSupplier: () =>
+                        new $MobEffectInstance(
+                            'critical_strike:damage',
+                            3600, /* 3 Minutes */
+                            1, /* Level 2 */
+                            true, /* Ambience */
+                            false /* Particles */
+                        ),
+                },
+                {
+                    probability: 1,
+                    effectSupplier: () =>
+                        new $MobEffectInstance(
+                            'minecraft:wither',
+                            3000, /* 2.5 Minutes */
+                            1, /* Level 2*/
+                            true, /* Ambience */
+                            false /* Particles */
+                        ),
+                },
+            ]
+        })
+    })
+})
